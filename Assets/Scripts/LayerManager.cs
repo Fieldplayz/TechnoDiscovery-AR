adddb13dfa26abdec973173d6e14c21119ade162
Fieldplayz <400042113@st.roc.a12.nl>
2024-03-13 11:00:21 +0100

    Added project

---
+++ Assets/Scripts/LayerManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LayerManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] Layers;
+    [SerializeField] TMP_Text longBrickCount;
+    [SerializeField] TMP_Text smallBrickCount;
+    [SerializeField] TMP_Text slopedBrickCount;
+    [SerializeField] TMP_Text roofCount;
+
+    int currentLayer;
+
+    int longBrickAmmount;
+    int smallBrickAmmount;
+    int slopedBrickAmmount;
+    int roofAmmount;
+
+    public void NextLayer()
+    {
+        longBrickAmmount = 0;
+        smallBrickAmmount = 0;
+        slopedBrickAmmount = 0;
+        roofAmmount = 0;
+
+        if (currentLayer < Layers.Length)
+        {
+            currentLayer++;
+
+            for (int i = 0; i < Layers.Length; i++)
+            {
+                if (Layers[i] != Layers[currentLayer])
+                {
+                    Layers[i].SetActive(false);
+                }
+                else
+                {
+                    Layers[i].SetActive(true);
+                }
+            }
+
+            for (int i = 0; i < Layers[currentLayer].transform.childCount; i++)
+            {
+                if (Layers[currentLayer].transform.GetChild(i).gameObject.CompareTag("LongBrick"))
+                {
+                    longBrickAmmount++;
+                }
+                else if (Layers[currentLayer].transform.GetChild(i).gameObject.CompareTag("SmallBrick"))
+                {
+                    smallBrickAmmount++;
+                }
+                else if (Layers[currentLayer].transform.GetChild(i).gameObject.CompareTag("SlopedBrick"))
+                {
+                    slopedBrickAmmount++;
+                }
+                else if (Layers[currentLayer].transform.GetChild(i).gameObject.CompareTag("Roof"))
+                {
+                    roofAmmount++;
+                }
+            }
+
+            longBrickCount.text = "Long Bricks: " + longBrickAmmount.ToString();
+            smallBrickCount.text = "Small Bricks: " + smallBrickAmmount.ToString();
+            slopedBrickCount.text = "Sloped Bricks: " + slopedBrickAmmount.ToString();
+            roofCount.text = "Roofs: " + roofAmmount.ToString();
+        }
+        else if(currentLayer == Layers.Length)
+        {
+            currentLayer = 0;
+
+            for (int i = 0; i < Layers.Length; i++)
+            {
+                Layers[i].SetActive(true);
+            }
+        }
+    }
+}
